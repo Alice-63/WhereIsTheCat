f201aeb2b16d1acbe8c57484b117993fd8387ed8
Alice-63 <alicetindag84@gmail.com>
2021-09-21 09:37:47 +0200

    add

---
+++ components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+ <div class="card">
+               <img :src="card.image" alt="">
+</div>
+    
+</template>
+
+<style scoped>
+.card
+{
+ width: 200px;
+ height: 232px;
+ border-radius: 5px;
+ border: 1px solid #666;
+ transition: box-shadow .5s;
+ margin-left: 15px;
+}
+.card:first-child
+{
+    margin-left: 0;
+}
+.card:hover
+{
+    box-shadow: 0px 5px 20px #666;
+    transition: box-shadow .5s;
+
+}
+</style>
+<script>
+export default {
+    props:
+    {
+        card:Object
+    }
+}
+</script>
---
+++ components/DefaultCard.vue
@@ -0,0 +1,29 @@
+<template>
+ <div class="card">
+           
+</div>
+    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.card
+{
+ width: 200px;
+ height: 232px;
+ border-radius: 5px;
+ border: 1px solid #666;
+ transition: box-shadow .5s;
+ /* margin-left: 15px;; */
+ background-image: url("../assets/background.jpg");
+ background-size: cover;
+ background-repeat: no-repeat;
+ background-position: center;
+ box-shadow: 0px 5px 20px #666;
+}
+
+
+</style>
